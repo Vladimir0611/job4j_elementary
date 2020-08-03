9edac53f97e153f9022dc13c3ce5960e4db9bad8
Владимир <tovlivanov@yandex.ru>
2020-08-03 12:31:44 +0400

    0. Что такое тестирование [#333916]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -3,12 +3,9 @@ package ru.job4j.converter;
 public class Converter {
     public static int rubleToEuro(int value) {
         return value / 70;
-
     }
-
     public static int rubleToDollar(int value) {
         return value / 35;
-
     }
 
     public static void main(String[] args) {
