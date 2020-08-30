06c7d252db41d072540eefa3c7de50caff9f18a8
Владимир <tovlivanov@yandex.ru>
2020-08-30 22:22:11 +0400

    5.5. Простое число [#333935]

--- src/ru/job4j/CheckPrimeNumber.java
+++ src/ru/job4j/CheckPrimeNumber.java
@@ -3,13 +3,16 @@ package ru.job4j;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
          boolean rsl = false;
-        for (int index = 2; index <= number - 1;) {
+         int index = 2;
+        while (index < number - 1) {
             if (number % index == 0) {
                 rsl = false;
+                index += 1;
+                break;
             } else {
-               rsl = true;
+                rsl = true;
+                index += 1;
             }
-            break;
         }
         return rsl;
     }
