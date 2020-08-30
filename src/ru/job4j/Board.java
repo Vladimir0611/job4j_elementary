56e8b94e45846e34bf058bc6e1d47b6fdfaaf04e
Владимир <tovlivanov@yandex.ru>
2020-08-30 16:30:04 +0400

    5.4. Протеиновая диета [#333932]

--- src/ru/job4j/Board.java
+++ src/ru/job4j/Board.java
@@ -3,8 +3,8 @@ package ru.job4j;
 public class Board {
     public static void paint(int width, int height) {
         int sum = 0;
-        for (int row = 1; row <= height; row++) {
-            for (int cell = 1; cell <= width; cell++) {
+        for (int row = 0; row < height; row++) {
+            for (int cell = 0; cell < width; cell++) {
                 sum = row + cell;
                 if (sum % 2 == 0) {
                     System.out.print("X");
