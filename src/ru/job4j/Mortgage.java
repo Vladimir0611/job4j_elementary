ce46a00a92bb62a42b38be3cfc554b58b25e2da4
Владимир <tovlivanov@yandex.ru>
2020-08-31 13:37:01 +0400

    5.7. Ипотека [#333934]

---
+++ src/ru/job4j/Mortgage.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+          amount = (int) (amount * (1 + (0.01 * percent)) - salary);
+          year++;
+        }
+        return year;
+    }
+}
