f3cba5bc9e5f07d71b412c70e9a9c8188781e82b
Владимир <tovlivanov@yandex.ru>
2020-09-07 10:20:25 +0400

    6.4. Слово начинается с ... [#333848]

--- src/ru/job4j/ArrayDefinition.java
+++ src/ru/job4j/ArrayDefinition.java
@@ -2,12 +2,6 @@ package ru.job4j;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер массива ages равен: " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер массива surnames равен: " + surnames.length);
-        float[] princes = new float[40];
-        System.out.println("Размер массива princes равен: " + princes.length);
 
         String[] names = new String[4];
         names[0] = "Alex Alexeev";
