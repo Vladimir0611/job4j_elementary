bcef96a0ff854a80534668b48137408ffe543577
Владимир <tovlivanov@yandex.ru>
2020-09-11 18:49:46 +0400

    1. Чтение кода и базовые ошибки [#334032]

--- src/ru/job4j/ru/job4j/condition/Max.java
+++ src/ru/job4j/ru/job4j/condition/Max.java
@@ -1,9 +1,24 @@
 package ru.job4j.ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
+    public static int max(int first, int second) {
         boolean condition = true;
-        int result = condition ? left : right;
-        return result;
+        return condition ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(
+                first,
+                max(second, third)
+        );
+    }
+
+    public static int max(int first, int second, int third, int four) {
+        return max(
+                first,
+                second,
+                max(third, four)
+        );
     }
 }
+
